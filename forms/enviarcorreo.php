502ea1fba1b32f30a0a5fb41a1ea834866c6081d
MauricioAlcala <a_mauricio@live.com>
2022-04-21 15:43:42 -0700

    Mail Form

---
+++ forms/enviarcorreo.php
@@ -0,0 +1,32 @@
+<?php
+
+require("../lib/PHPMailer/src/PHPMailer.php");
+require("../lib/PHPMailer/src/SMTP.php");
+
+ $mail = new PHPMailer\PHPMailer\PHPMailer();
+ $mail->IsSMTP(); // enable SMTP
+ $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+ $mail->SMTPAuth = true; // authentication enabled
+ $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+ $mail->Host = "mail.acuabar.com";
+ $mail->Port = 465; //465  or 587
+ $mail->IsHTML(true);
+ $mail->Username = "contacto@acuabar.com";
+ $mail->Password = "Rosaritocentro1";
+ $mail->SetFrom("contacto@acuabar.com");
+ $mail->Subject = "Contacto Desde : Mi pagina web";
+ 
+ $name = $_POST['name'];
+ $email = $_POST['email'];
+ $subject = $_POST['subject'];
+ $message = $_POST['message'];
+ 
+ $mail->Body = "Nombre: {$name} <br> Email: {$email} <br> Asunto : {$subject} <br> Mensaje : {$message}";
+ $mail->AddAddress("contacto@acuabar.com");
+ if(!$mail->Send()) {
+ echo "Mailer Error: " . $mail->ErrorInfo;
+ } else {
+ echo "OK";
+ }
+
+ ?>
